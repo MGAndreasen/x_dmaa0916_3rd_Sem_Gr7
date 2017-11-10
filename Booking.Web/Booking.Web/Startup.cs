5e1bf2f717db3e414105a778a03424bcd6bfe87d
Michael Gerner Andreasen <geekcamp@gmail.com>
2017-11-10 12:10:19 +0100

    Initial Solutions

---
+++ Booking.Web/Booking.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Booking.Web.Startup))]
+namespace Booking.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
