d71350ebd587519199a792bb372e07988b2e91a6
Arnes Kovacevic <1061725@ucn.dk>
2017-11-15 12:19:18 +0100

    dbaccess

---
+++ Booking.Service/Booking.DB/DataAccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Data.Sql;
+
+namespace Booking.DB
+{
+    public class DataAccess
+    {
+        private string connectionString;
+
+        public DataAccess()
+        {
+            this.connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+
+    }
+}
