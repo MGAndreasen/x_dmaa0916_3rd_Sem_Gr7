b1dfc2b53cd74ab914b30fac87754d0797bda39a
Michael Gerner Andreasen <geekcamp@gmail.com>
2017-11-16 12:03:44 +0100

    Changed InstanceContexMode to PerCall

--- Booking.Service/Booking.Service/Service.cs
+++ Booking.Service/Booking.Service/Service.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 namespace Booking.Service
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
     public class Service : IService
     {
         //public string GetData(int value)
