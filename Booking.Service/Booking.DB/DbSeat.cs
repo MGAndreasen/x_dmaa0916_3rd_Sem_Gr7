ee097ebdc72985e1d460e399dd5ee88f2b40cb1d
Jacob <Jacob@JACOBG>
2017-11-17 08:56:54 +0100

    Create Seat (id1) start gg

--- Booking.Service/Booking.DB/DbSeat.cs
+++ Booking.Service/Booking.DB/DbSeat.cs
@@ -8,5 +8,24 @@ namespace Booking.DB
 {
     public class DbSeat
     {
+
+
+        public void CreateSeat(int ID, int Number, bool Available)
+        {
+            //Open
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                try
+                {
+                    cmd.CommandText = "Insert into dbo.Seat(ID, Number, Avaliable)";
+                }
+
+                catch
+                {
+                    Console.WriteLine("Den gider sgu ikke lige");
+                }
+                //open
+            }
+        }
     }
 }
