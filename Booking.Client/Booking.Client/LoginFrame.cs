bcbe7748625ff740140d99c49cef63311e926dfc
Arnes Kovacevic <1061725@ucn.dk>
2017-11-21 11:00:37 +0100

    Lige noget her og der kh arne k the one and only aka balkankriger aka balkantank aka balkanwarrior aka the man aka the tank aka the monkeymaker aka the legend aka dubår kongen aka KONGEN aka BalkanKing

---
+++ Booking.Client/Booking.Client/LoginFrame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Booking.Client
+{
+    public partial class LoginFrame : Form
+    {
+        public LoginFrame()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             //Hides LoginFrame (The main applicantion)
+            this.Hide();
+
+            //Starts a new window
+            MainFrame MF = new MainFrame();
+            MF.ShowDialog();
+
+            //Closes the LoginFrame after the other window has opened. 
+            this.Close();
+
+
+            
+        }
+
+    }
+}
