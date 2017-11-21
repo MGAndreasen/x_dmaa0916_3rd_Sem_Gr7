da698b54c01f60880254d569e4be71c6a42d08a9
Arnes Kovacevic <1061725@ucn.dk>
2017-11-21 11:10:15 +0100

    Rettelser af fejl under DB

--- Booking.Service/Booking.DB/DBCustomer.cs
+++ Booking.Service/Booking.DB/DBCustomer.cs
@@ -46,6 +46,8 @@ namespace Booking.DB
 
                     cmd.ExecuteNonQuery();
 
+                    scope.Complete();
+
                 }
             }
         }
--- Booking.Service/Booking.DB/DbBooking.cs
+++ Booking.Service/Booking.DB/DbBooking.cs
@@ -30,23 +30,21 @@ namespace Booking.DB
         public void Create(Bookings obj)
         {
             TransactionOptions isoLevel = new TransactionOptions { IsolationLevel = System.Transactions.IsolationLevel.ReadCommitted };//her kan i sætte isolation om nødvendigt
-            using (scope = new TransactionScope(TransactionScopeOption.Required, isoLevel))
+            using (TransactionScope scope = new TransactionScope(TransactionScopeOption.Required, isoLevel))
             { 
                 using (SqlConnection con = new SqlConnection(data.GetConnectionString()))
                 { 
                     con.Open();
                     SqlCommand cmd = new SqlCommand("INSERT INTO dbo.Booking_Booking (@id, @StartDestination, @EndDestination, @Date, @Price", con);
-                    cmd.Parameters.Add("@id", SqlDbType.Int).Value = variable;
-                    cmd.Parameters.Add("@StartDestination", SqlDbType.VarChar).Value = variable;
-                    cmd.Parameters.Add("@EndDestination", SqlDbType.VarChar).Value = variable;
-                    cmd.Parameters.Add("@Date", SqlDbType.DateTime).Value = variable;
-                    cmd.PArameters.Add("@Price", SqlDbType.Int).Value = variables;
-                    // Har ikke adgang til Models.Booking, så en eller anden sæt lige variables ind hehe, kh j
+                    cmd.Parameters.Add("@id", SqlDbType.Int).Value = obj.Id;
+                    cmd.Parameters.Add("@StartDestination", SqlDbType.VarChar).Value = obj.StartDestination;
+                    cmd.Parameters.Add("@EndDestination", SqlDbType.VarChar).Value = obj.EndDestination;
+                    cmd.Parameters.Add("@Date", SqlDbType.DateTime).Value = obj.Date;
+                    cmd.Parameters.Add("@Price", SqlDbType.Int).Value = obj.TotalPrice;
+
                     cmd.ExecuteNonQuery();
-                    {
-                        //tilføj til model.
-                    }
-                    scope.Complete(); //pas hvornår den skal stå...??
+                  
+                    scope.Complete(); 
                 }
             }
         }
