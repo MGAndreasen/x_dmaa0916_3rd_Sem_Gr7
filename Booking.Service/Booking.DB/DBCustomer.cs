c21bab04be30f09f858cf1d4f52cb3f285ae2a52
Arnes Kovacevic <1061725@ucn.dk>
2017-11-21 11:26:10 +0100

    Merge Error Fix

--- Booking.Service/Booking.DB/DBCustomer.cs
+++ Booking.Service/Booking.DB/DBCustomer.cs
@@ -21,59 +21,61 @@ namespace Booking.DB
 
         public Customer Get(int id)
         {
-                using (SqlConnection con = new SqlConnection(data.GetConnectionString()))
+            using (SqlConnection con = new SqlConnection(data.GetConnectionString()))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT s.Id, s.Cpr, s.PhoneNo, s.Zipcode, s.FirstName, s.LastName, s.Email, s.Address, s.Password, s.Comfirmed FROM dbo.Booking_Customer AS s WHERE Id = @id", con);
+                cmd.Parameters.Add("@Id", SqlDbType.Int).Value = id;
+                SqlDataReader reader = cmd.ExecuteReader();
+                return new Customer
                 {
-                    con.Open();
-                    SqlCommand cmd = new SqlCommand("SELECT s.Id, s.Cpr, s.PhoneNo, s.Zipcode, s.FirstName, s.LastName, s.Email, s.Address, s.Password, s.Comfirmed FROM dbo.Booking_Customer AS s WHERE Id = @id", con);
-                    cmd.Parameters.Add("@Id", SqlDbType.Int).Value = id;
-                    SqlDataReader reader = cmd.ExecuteReader();
-                    return new Customer
-                    {
-                        Id = reader.GetInt32(0),
-                        CPR = reader.GetInt32(1),
-                        PhoneNumber = reader.GetInt32(2),
-                        //zipcode
-                        FirstName = reader.GetString(4),
-                        LastName = reader.GetString(5),
-                        Email = reader.GetString(6),
-                        Address = reader.GetString(7),
-                        Password = reader.GetString(8),
-                        Confirmed = reader.GetBoolean(9)
-                    };
-                }
+                    Id = reader.GetInt32(0),
+                    CPR = reader.GetInt32(1),
+                    PhoneNumber = reader.GetInt32(2),
+                    //zipcode
+                    FirstName = reader.GetString(4),
+                    LastName = reader.GetString(5),
+                    Email = reader.GetString(6),
+                    Address = reader.GetString(7),
+                    Password = reader.GetString(8),
+                    Confirmed = reader.GetBoolean(9)
+                };
+            }
         }
 
         public void Create(Customer obj)
         {
-            try
-            {
-                TransactionOptions isoLevel = new TransactionOptions { IsolationLevel = System.Transactions.IsolationLevel.ReadCommitted };//her kan i sætte isolation om nødvendigt
-                using (TransactionScope scope = new TransactionScope(TransactionScopeOption.Required, isoLevel))
-                {
-                    using (SqlConnection con = new SqlConnection(data.GetConnectionString()))
-                    {
-                        con.Open();
-                        SqlCommand cmd = new SqlCommand("INSERT INTO dbo.Booking_Customer (@Id, @Cpr, @PhoneNo, @Zipcode, @FirstName, @LastName, @Email. @Address, @Password, @Comfirmed)", con);
-                        cmd.Parameters.Add("@Id", SqlDbType.Int).Value = obj.Id;
-                        cmd.Parameters.Add("@Cpr", SqlDbType.BigInt).Value = obj.CPR;
-                        cmd.Parameters.Add("@PhoneNo", SqlDbType.BigInt).Value = obj.PhoneNumber;
-                        cmd.Parameters.Add("@Zipcode", SqlDbType.SmallInt).Value = obj.City;
-                        cmd.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = obj.FirstName;
-                        cmd.Parameters.Add("@LastName", SqlDbType.VarChar).Value = obj.LastName;
-                        cmd.Parameters.Add("@Email", SqlDbType.VarChar).Value = obj.Email;
-                        cmd.Parameters.Add("@Address", SqlDbType.VarChar).Value = obj.Address;
-                        cmd.Parameters.Add("@Password", SqlDbType.VarChar).Value = obj.Password;
-                        cmd.Parameters.Add("@Comfirmed", SqlDbType.Bit).Value = obj.Confirmed;
 
-                        cmd.ExecuteNonQuery();
+            TransactionOptions isoLevel = new TransactionOptions { IsolationLevel = System.Transactions.IsolationLevel.ReadCommitted };//her kan i sætte isolation om nødvendigt
+            using (TransactionScope scope = new TransactionScope(TransactionScopeOption.Required, isoLevel))
+            {
+                using (SqlConnection con = new SqlConnection(data.GetConnectionString()))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO dbo.Booking_Customer (@Id, @Cpr, @PhoneNo, @Zipcode, @FirstName, @LastName, @Email. @Address, @Password, @Comfirmed)", con);
+                    cmd.Parameters.Add("@Id", SqlDbType.Int).Value = obj.Id;
+                    cmd.Parameters.Add("@Cpr", SqlDbType.BigInt).Value = obj.CPR;
+                    cmd.Parameters.Add("@PhoneNo", SqlDbType.BigInt).Value = obj.PhoneNumber;
+                    cmd.Parameters.Add("@Zipcode", SqlDbType.SmallInt).Value = obj.City;
+                    cmd.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = obj.FirstName;
+                    cmd.Parameters.Add("@LastName", SqlDbType.VarChar).Value = obj.LastName;
+                    cmd.Parameters.Add("@Email", SqlDbType.VarChar).Value = obj.Email;
+                    cmd.Parameters.Add("@Address", SqlDbType.VarChar).Value = obj.Address;
+                    cmd.Parameters.Add("@Password", SqlDbType.VarChar).Value = obj.Password;
+                    cmd.Parameters.Add("@Comfirmed", SqlDbType.Bit).Value = obj.Confirmed;
+
+                    cmd.ExecuteNonQuery();
 
                 }
             }
+
         }
+
 
         public void Update(int id)
         {
             throw new NotImplementedException();
         }
+
     }
 }
--- Booking.Service/Booking.DB/DbBooking.cs
+++ Booking.Service/Booking.DB/DbBooking.cs
@@ -51,7 +51,25 @@ namespace Booking.DB
 
         public void Update(int id)
         {
-            throw new NotImplementedException();
+            TransactionOptions isoLevel = new TransactionOptions { IsolationLevel = System.Transactions.IsolationLevel.ReadCommitted };//her kan i sætte isolation om nødvendigt
+            using (TransactionScope scope = new TransactionScope(TransactionScopeOption.Required, isoLevel))
+            {
+                using (SqlConnection con = new SqlConnection(data.GetConnectionString()))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT FROM dbo.Booking_Booking Id();
+                    cmd.Parameters.Add("@id", SqlDbType.Int).Value = obj.Id;
+                    cmd.Parameters.Add("@StartDestination", SqlDbType.VarChar).Value = obj.StartDestination;
+                    cmd.Parameters.Add("@EndDestination", SqlDbType.VarChar).Value = obj.EndDestination;
+                    cmd.Parameters.Add("@Date", SqlDbType.DateTime).Value = obj.Date;
+                    cmd.Parameters.Add("@Price", SqlDbType.Int).Value = obj.TotalPrice;
+
+                    cmd.ExecuteNonQuery();
+
+                    scope.Complete();
+                }
+            }
         }
+    }
     }
 }
