20971a2d8072a9e1f583bea4bfc52a5ffc545096
Michael Gerner Andreasen <geekcamp@gmail.com>
2017-11-22 09:59:49 +0100

    City done

--- Booking.Service/Booking.Test/DBCity.cs
+++ Booking.Service/Booking.Test/DBCity.cs
@@ -7,7 +7,7 @@ namespace Booking.Test
     public class DBCity
     {
         [TestMethod()]
-        public void Create()
+        public void CityCreate()
         {
             Booking.Models.City c = new Models.City(9999, "Test");
             Booking.DB.DbCity dbCity = new DB.DbCity();
@@ -18,34 +18,63 @@ namespace Booking.Test
 
             dbCity.Create(c);
 
-            Assert.AreEqual(c, dbCity.Get(9999));
+
+            
+            Assert.AreEqual(c.Zipcode, dbCity.Get(9999).Zipcode);
 
 
             
         }
 
-        public void Read()
+        [TestMethod()]
+        public void CityRead()
         {
+            Booking.Models.City c = new Models.City(9999, "Test");
+            Booking.DB.DbCity dbCity = new DB.DbCity();
 
+            dbCity.Delete(9999);
+            dbCity.Create(c);
 
+            Assert.IsNotNull(dbCity.Get(9999));
+        }
 
+        [TestMethod()]
+        public void CityUpdate()
+        {
+            Booking.Models.City c = new Models.City(9999, "Test");
+            Booking.DB.DbCity dbCity = new DB.DbCity();
+
+            dbCity.Delete(9999);
+            dbCity.Create(c);
+
+            Assert.AreEqual(dbCity.Get(9999).CityName , "Test");
+
+            c.CityName = "Changed";
+
+            dbCity.Update(c);
+
+            Assert.AreEqual(dbCity.Get(9999).CityName, "Changed");
 
         }
 
-        public void Update()
+        [TestMethod()]
+        public void CityDelete()
         {
+            Booking.Models.City c = new Models.City(9999, "Test");
+            Booking.DB.DbCity dbCity = new DB.DbCity();
 
+            dbCity.Delete(9999);
+            dbCity.Create(c);
 
-
-
-        }
-
-        public void Delete()
-        {
-
-
-
-
+            if (dbCity.Get(9999).Zipcode == c.Zipcode)
+            {
+                dbCity.Delete(9999);
+                Assert.IsNull(dbCity.Get(9999));
+            }
+            else
+            {
+                Assert.Fail();
+            }
         }
     }
 }
