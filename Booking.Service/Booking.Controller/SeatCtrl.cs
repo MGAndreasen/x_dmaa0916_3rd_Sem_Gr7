29da6ff850cad5b5039de5379dec5193aa0b810c
Philip Heidemann Jensen <1061973@ucn.dk>
2017-11-23 11:22:33 +0100

    seatctrl

--- Booking.Service/Booking.Controller/SeatCtrl.cs
+++ Booking.Service/Booking.Controller/SeatCtrl.cs
@@ -4,10 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Booking.Models;
+using Booking.DB;
 
 namespace Booking.Controller
 {
-    class SeatCtrl
+    public class SeatCtrl : ICRUD<Seat>
     {
+        private static DbSeat dbs = new DbSeat();
+        public void Create(Seat obj)
+        {
+            obj = new Seat();
+            dbs.Create(obj);
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Seat Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
