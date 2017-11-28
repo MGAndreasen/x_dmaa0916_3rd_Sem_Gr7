e539d7579e76a321216edcbd290475b4d2432ea9
Michael Gerner Andreasen <geekcamp@gmail.com>
2017-11-28 23:35:53 +0100

    fix

--- Booking.Service/Booking.Models/Ticket.cs
+++ Booking.Service/Booking.Models/Ticket.cs
@@ -1,25 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Booking.Models
 {
+    [DataContract]
     public class Ticket
     {
-        public int Id { get; set; }
-        public Passenger Passenger { get; set; }
-
         // Constructors
+
+        public Ticket()
+        {
+        }
+
         public Ticket(int id, Passenger p)
         {
             Id = id;
             Passenger = p;
         }
 
-        public Ticket()
-        {
-        }
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public Passenger Passenger { get; set; }
     }
 }
