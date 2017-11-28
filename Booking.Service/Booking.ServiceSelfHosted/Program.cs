633ee609718b9663f48e4444f149314e2e5ae454
Michael Gerner Andreasen <geekcamp@gmail.com>
2017-11-28 13:57:22 +0100

    mycert i c:\ roden

--- Booking.Service/Booking.ServiceSelfHosted/Program.cs
+++ Booking.Service/Booking.ServiceSelfHosted/Program.cs
@@ -13,8 +13,7 @@ namespace Booking.ServiceSelfHosted
     {
         static void Main(string[] args)
         {
-
-            var certificate = new X509Certificate2("/MyCert.pfx", "1234Qwer!");
+            var certificate = new X509Certificate2("c:\\MyCert.pfx", "1234Qwer!");
 
             //This will host both wsHttp and netTcp bindings of secureservice
             ServiceHost host = new ServiceHost(typeof(Booking.Service.Service));
