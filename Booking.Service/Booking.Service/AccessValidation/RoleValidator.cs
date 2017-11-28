c970a3eafe7ab8a9f1e5423e4e7f6b360f40f246
Michael Gerner Andreasen <geekcamp@gmail.com>
2017-11-28 23:43:25 +0100

    hmm

--- Booking.Service/Booking.Service/AccessValidation/RoleValidator.cs
+++ Booking.Service/Booking.Service/AccessValidation/RoleValidator.cs
@@ -17,14 +17,18 @@ namespace Booking.Service.AccessValidation
             //Get the current pipeline user context
             var identity = operationContext.ServiceSecurityContext.PrimaryIdentity;
             //simulate that we get a user and all his roles from the database
+
             var userFound = uCtrl.GetUser(identity.Name);
-            string[] userRolesFound = userFound.Roles.Select(x => x.Name).ToArray();
+            
             if (userFound == null)
             {
-                throw new Exception("User not found");
+                //throw new Exception("User not found");
+
+                return false;
             }
             else
             {
+                string[] userRolesFound = userFound.Roles.Select(x => x.Name).ToArray();
                 //Assign roles to the Principal property for runtime to match with PrincipalPermissionAttributes decorated on the service operation.
                 var principal = new GenericPrincipal(operationContext.ServiceSecurityContext.PrimaryIdentity, userRolesFound);
                 //assign principal to auth context with the users roles
