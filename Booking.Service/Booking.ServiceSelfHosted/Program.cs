ec97bd6cb7e2205b6c34e81c08d71eca03731c45
Michael Gerner Andreasen <geekcamp@gmail.com>
2017-11-28 13:52:32 +0100

    dsadas

--- Booking.Service/Booking.ServiceSelfHosted/Program.cs
+++ Booking.Service/Booking.ServiceSelfHosted/Program.cs
@@ -13,6 +13,7 @@ namespace Booking.ServiceSelfHosted
     {
         static void Main(string[] args)
         {
+
             var certificate = new X509Certificate2("/MyCert.pfx", "1234Qwer!");
 
             //This will host both wsHttp and netTcp bindings of secureservice
