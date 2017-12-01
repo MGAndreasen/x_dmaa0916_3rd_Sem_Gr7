d2750be9d543bf2dda08f8d539b8f00655d438d7
Philip Heidemann Jensen <1061973@ucn.dk>
2017-12-01 10:58:46 +0100

    fæx.

--- Booking.Service/Booking.Models/City.cs
+++ Booking.Service/Booking.Models/City.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.Serialization;
 
 namespace Booking.Models
 {
@@ -30,10 +25,5 @@ namespace Booking.Models
         [DataMember]
         public string CityName { get; set; }
 
-        
-        public override string ToString()
-        {
-            return Zipcode.ToString() + " " + CityName.ToString() + " ";
-        }
     }
 }
--- Booking.Service/Booking.Models/Destination.cs
+++ Booking.Service/Booking.Models/Destination.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.Serialization;
 
 namespace Booking.Models
 {
@@ -23,9 +18,5 @@ namespace Booking.Models
         [DataMember]
         public string NameDestination { get; set; }
 
-        public override string ToString()
-        {
-            return NameDestination.ToString() + " ";
-        }
     }
 }
