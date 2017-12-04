0579875db2302d77c3aad87c7499532691c23aee
Michael Gerner Andreasen <geekcamp@gmail.com>
2017-12-04 12:07:28 +0100

    auth fixes

---
+++ Booking.Web/Booking.Web/Helpers/AuthHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Booking.Web.Models;
+
+namespace Booking.Web.Helpers
+{
+    public class AuthHelper
+    {
+        private static string LoginSessionName = "LoggedInUser";
+        private static LoginViewModel currentUser;
+        public static LoginViewModel CurrentUser
+        {
+            get
+            {
+                return HttpContext.Current.Session[LoginSessionName] as LoginViewModel;
+            }
+            set
+            {
+                currentUser = value;
+            }
+        }
+
+
+        public static bool IsLoggedIn()
+        {
+            return HttpContext.Current.Session[LoginSessionName] != null;
+        }
+        public static void Login(LoginViewModel lvm)
+        {
+            HttpContext.Current.Session[LoginSessionName] = lvm;
+        }
+        public static void Logout()
+        {
+            HttpContext.Current.Session[LoginSessionName] = null;
+        }
+
+    }
+}
--- Booking.Web/Booking.Web/Startup.cs
+++ Booking.Web/Booking.Web/Startup.cs
@@ -1,14 +1,14 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿//using Microsoft.Owin;
+//using Owin;
 
-[assembly: OwinStartupAttribute(typeof(Booking.Web.Startup))]
-namespace Booking.Web
-{
-    public partial class Startup
-    {
-        public void Configuration(IAppBuilder app)
-        {
-            ConfigureAuth(app);
-        }
-    }
-}
+//[assembly: OwinStartupAttribute(typeof(Booking.Web.Startup))]
+//namespace Booking.Web
+//{
+//    public partial class Startup
+//    {
+//        public void Configuration(IAppBuilder app)
+//        {
+//            //ConfigureAuth(app);
+//        }
+//    }
+//}
