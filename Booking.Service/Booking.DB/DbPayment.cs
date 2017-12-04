3af9069119d8f48502acba92a69e37fec160bd83
Michael Gerner Andreasen <geekcamp@gmail.com>
2017-12-04 22:54:23 +0100

    fix

--- Booking.Service/Booking.DB/DbPayment.cs
+++ Booking.Service/Booking.DB/DbPayment.cs
@@ -22,7 +22,7 @@ namespace Booking.DB
             using (SqlConnection con = new SqlConnection(data.GetConnectionString()))
                 {
                 con.Open();
-                SqlCommand cmd = new SqlCommand("SELECT FROM dbo.Booking_Payment WHERE Id = @id", con);
+                SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.Booking_Payment WHERE Id = @id", con);
                 cmd.Parameters.Add("@Id", SqlDbType.Int).Value = id;
                 SqlDataReader rdr = cmd.ExecuteReader();
                 rdr.Read();
