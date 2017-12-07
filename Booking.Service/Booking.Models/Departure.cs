17d388173a587f29e13bd5788dd065158fba4ec1
Arnes Kovacevic <1061725@ucn.dk>
2017-12-07 14:03:21 +0100

    Departure model

---
+++ Booking.Service/Booking.Models/Departure.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Models
+{
+    [DataContract]
+    public class Departure
+    {
+        [DataMember]
+        public string StartDestination { get; set; }
+        [DataMember]
+        public string EndDestination { get; set; }
+        [DataMember]
+        public DateTime DepartureTime { get; set; }
+        [DataMember]
+        public Plane PlaineId { get; set; }
+
+        public Departure()
+        {
+            //Constructure
+        }
+    }
+}
