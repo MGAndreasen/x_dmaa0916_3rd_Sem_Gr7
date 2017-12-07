df52f888fe76f713ccf9d1f425f8c2a7b83db77f
Arnes Kovacevic <1061725@ucn.dk>
2017-12-07 14:35:01 +0100

    DbDeparture

---
+++ Booking.Service/Booking.Controller/DepartureCtrl.cs
@@ -0,0 +1,32 @@
+﻿using Booking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Controller
+{
+    public class DepartureCtrl : ICRUD<Departure>
+    {
+        public void Create(Departure obj)
+        {
+            
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Departure Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Departure obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
