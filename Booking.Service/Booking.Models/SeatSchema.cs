6b40a848d7367d08a218f2094aa0645f8c9436b5
Michael Gerner Andreasen <geekcamp@gmail.com>
2017-12-11 09:55:43 +0100

    Fjern Plain_Id fra seat schema

--- Booking.Service/Booking.Models/SeatSchema.cs
+++ Booking.Service/Booking.Models/SeatSchema.cs
@@ -13,8 +13,8 @@ namespace Booking.Models
         public int Id { get; set; }
 
         // Burde vi fjerne, da vi faktisk godt ved hvilket plane et schea høre til, hvis der holdes styr på plane_seatschema listen
-        [DataMember]
-        public int PlaneId { get; set; }
+        //[DataMember]
+        //public int PlaneId { get; set; }
 
         [DataMember]
         public int Row { get; set; }
