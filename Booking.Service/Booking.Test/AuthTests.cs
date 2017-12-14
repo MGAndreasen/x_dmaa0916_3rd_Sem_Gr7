d982a0784af343efe7b6bc95a989874197ada29d
Michael Gerner Andreasen <geekcamp@gmail.com>
2017-12-14 12:48:26 +0100

    fix

---
+++ Booking.Service/Booking.Test/AuthTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Booking.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Booking.Test.AuthRemote;
+
+namespace Booking.Service.Tests
+{
+    [TestClass()]
+    public class AuthTests
+    {
+        AuthClient client = new AuthClient();
+
+        [TestMethod()]
+        public void CreateLoginTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoginTest()
+        {
+            Assert.IsNull(client.Login("test", "svend"));
+            Assert.IsNotNull(client.Login("admin@test.dk", "1234"));
+        }
+    }
+}
---
+++ Booking.Web/Booking.Web/Models/Booking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Booking.Web.Models
+{
+    public class Booking
+    {
+        public int Departure { get; set; }
+        public int Customer { get; set; }
+
+    }
+}
